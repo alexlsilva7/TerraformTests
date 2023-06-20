64cb540f6b3b25ea539982689de6314fe953d0b8
Alex Lopes <alex.lsilva154@gmail.com>
2023-06-20 17:13:00 -0300

    inicio do curso de separação de ambientes na aws

---
+++ alura-separando-ambientes-aws/infra/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+
+provider "aws" {
+  profile = "default"
+  region = var.regiao_aws
+
+}
+
+resource "aws_instance" "app_server" {
+  ami = "ami-053b0d53c279acc90" // Ubuntu Server 22.04 LTS
+  instance_type = "var.instancia"
+  key_name = var.chave
+
+  tags = {
+    Name = "Terraform Ansible Python"
+  }
+}
+
+resource "aws_key_pair" "chaveSSH" {
+  key_name = var.chave
+  public_key = file("${var.chave}.pub")
+}
