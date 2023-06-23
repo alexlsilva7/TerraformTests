19bb3e16a6ddb96f6e216c08186bc9836eca64a3
Alex Lopes <alex.lsilva154@gmail.com>
2023-06-23 15:56:34 -0300

    configuração do ECR

--- alura-docker-ecs/env/prod/main.tf
+++ alura-docker-ecs/env/prod/main.tf
@@ -1,9 +1,4 @@
 module "prod" {
   source = "../../infra"
-  instance_type = "t2.micro"
-  min_size = 2
-  max_size = 5
-  name = "docker-ecs-prod"
-  description = "aplicação de produção do curso de Infraestrutura como código: Terraform, Docker e Elastic Container Service da Alura"
-  environment_name = "prod-env"
+  repository_name = "alura-docker-ecs"
 }
--- alura-docker-ecs/infra/Variables.tf
+++ alura-docker-ecs/infra/Variables.tf
@@ -1,23 +1,3 @@
-variable "name" {
+variable "repository_name" {
   type = string
-}
-
-variable "description" {
-  type = string
-}
-
-variable "environment_name" {
-  type = string
-}
-
-variable "instance_type" {
-  type = string
-}
-
-variable "max_size" {
-  type = number
-}
-
-variable "min_size" {
-  type = number
 }
