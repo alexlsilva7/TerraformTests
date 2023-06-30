9ad88f290b8fe71efa7d0e40ce7ede74824e4bab
Alex Lopes <alex.lsilva154@gmail.com>
2023-06-30 09:52:54 -0300

    configurações do load balancer

--- alura-docker-ecs/env/prod/main.tf
+++ alura-docker-ecs/env/prod/main.tf
@@ -3,3 +3,7 @@ module "prod" {
   repository_name = "alura-docker-ecs"
   iam_role_name = "alura-docker-ecs-prod"
 }
+
+output "lb_dns" {
+  value = module.prod.load_balancer_dns
+}
---
+++ alura-docker-ecs/infra/LoadBalancer.tf
@@ -0,0 +1,29 @@
+resource "aws_lb" "load_balancer" {
+  name               = "alura-docker-ecs-django"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.load_balancer.id]
+  subnets            = module.vpc.public_subnets
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port              = 8000
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.target_group.arn
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name     = "alura-docker-ecs-django"
+  port     = 8000
+  protocol = "HTTP"
+  target_type = "ip"
+  vpc_id   = module.vpc.vpc_id
+}
+
+output "load_balancer_dns" {
+  value = aws_lb.load_balancer.dns_name
+}
