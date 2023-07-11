fd93cf937549f11d396215e45ff67034da83e519
Alex Lopes <alex.lsilva154@gmail.com>
2023-07-11 11:06:16 -0300

    task

--- alura-docker-ecs/infra/ECS.tf
+++ alura-docker-ecs/infra/ECS.tf
@@ -11,3 +11,27 @@
     }
   ]
  }
+
+resource "aws_ecs_task_definition" "app" {
+  family = "Django-API"
+  requires_compatibilities = ["FARGATE"]
+  network_mode = "awsvpc"
+  cpu = 256
+  memory = 512
+  execution_role_arn = aws_iam_role.role.arn
+  container_definitions = jsonencode([
+    {
+      "name" = var.environment
+      "image" = "${aws_ecr_repository.repository.repository_url}/producao:v1"
+      "cpu" = 256
+      "memory" = 512
+      "essential" = true
+      "portMappings" = [
+        {
+          "containerPort" = 8000
+          "hostPort" = 8000
+        }
+      ]
+    }
+  ])
+}
